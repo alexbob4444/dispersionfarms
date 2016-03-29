14f6407acb01e9730657b45f4826f89c5a87f5dd
alexbob4444 <alexbob4444@gmail.com>
2016-03-29 11:50:13 -0500

    Update updatesite.php

--- updatesite.php
+++ updatesite.php
@@ -1,4 +1,5 @@
 <?php
 exec(dirname(/home/pi/Desktop/dispersionfarms/) 'chmod +x shellupdate');
 exec(dirname(/home/pi/Desktop/dispersionfarms/) . '/shellupdate');
+echo('The code ran and maybe worked...');
 ?>
