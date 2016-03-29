db148bbdeb101c9c4404673e111aaa38d8978e0c
alexbob4444 <alexbob4444@gmail.com>
2016-03-29 16:06:08 +0000

    updated php scripts

--- updatesite.php
+++ updatesite.php
@@ -1,6 +1,5 @@
 <?php
 shell_exec('cd /');
-shell_exec('chmod +x /home/pi/Desktop/dispersionfarms/shellupdate');
-shell_exec('./home/pi/Desktop/dispersionfarms/shellupdate');
-
+shell_exec('sudo chmod +x /home/pi/Desktop/dispersionfarms/shellupdate');
+shell_exec('sudo ./home/pi/Desktop/dispersionfarms/shellupdate');
 ?>
