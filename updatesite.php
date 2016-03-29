76f712ddcaed7334e123776927d54f4a57a04c40
alexbob4444 <alexbob4444@gmail.com>
2016-03-29 11:44:33 -0500

    Update updatesite.php

--- updatesite.php
+++ updatesite.php
@@ -1,5 +1,5 @@
 <?php
-shell_exec('cd /home/pi/Desktop/dispersionfarms/');
-shell_exec('chmod +x shellupdate');
-shell_exec('sudo ./shellupdate');
+exec('cd /home/pi/Desktop/dispersionfarms/');
+exec('chmod +x shellupdate');
+exec('sudo ./shellupdate');
 ?>
