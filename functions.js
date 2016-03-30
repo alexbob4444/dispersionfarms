451f7cef51c857b7198d544e03c5d09bd071a86a
alexbob4444 <alexbob4444@gmail.com>
2016-03-30 13:43:54 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,11 +1,9 @@
-$(document).ready(function()
-{
-    $(window).resize(function() {
-        if (screen.width<700) {//screen of mobile
-          document.getElementById("headerlogo").className = "headerlogom";
-        }
-        else {//screen of desktop
-          document.getElementById("headerlogo").className = "headerlogod";
-        }
-    });
+
+$(window).resize(function() {
+    if (screen.width<700) {//screen of mobile
+      document.getElementById("headerlogo").className = "headerlogom";
+    }
+    else {//screen of desktop
+      document.getElementById("headerlogo").className = "headerlogod";
+    }
 });
