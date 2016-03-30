ca33f94cf9cb4620926fdea1cc71ae9a6022e59e
alexbob4444 <alexbob4444@gmail.com>
2016-03-30 16:46:40 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 function eventResize() {
     if (window.innerWidth<600) {//screen of mobile
       document.getElementById("headerlogo").className = "headerlogom";
-      document.getElementById("menuicon").style.padding = '20px';
+      document.getElementById("menuicon").className = 'menuiconm';
     }
     else {//screen of desktop
       document.getElementById("headerlogo").className = "headerlogod";
-      document.getElementById("menuicon").style.padding = '30px';
+      document.getElementById("menuicon").className = 'menuicond';
     }
     document.getElementById("page").width=(window.innerWidth+50);
 }
