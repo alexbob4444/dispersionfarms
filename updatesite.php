4dd2fb3276bbb44d66dfb2aee34669db66b38291
alexbob4444 <alexbob4444@gmail.com>
2016-03-30 07:19:03 -0500

    Update updatesite.php

--- updatesite.php
+++ updatesite.php
@@ -1,7 +1,7 @@
 <?php
 echo('0The code ran and maybe worked... ');
 echo shell_exec("/var/www/html/dispersionfarms/ git pull 2>&1");
-echo shell_exec('/var/www/html/dispersionfarms/ chmod +x shellupdate');
-echo shell_exec('/var/www/html/dispersionfarms/ ./shellupdate');
+echo shell_exec('/ cp /var/www/html/dispersionfarms/* /var/www/html/');
+echo shell_exec('/ cp /var/www/html/dispersionfarms/images/* /var/www/html/images/');
+echo shell_exec('/ cp /var/www/html/dispersionfarms/textinfo/* /var/www/html/textinfo');
 ?>
-
