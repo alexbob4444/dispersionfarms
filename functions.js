36e032cc64d01153c6450f784ca6416b81ce7380
alexbob4444 <alexbob4444@gmail.com>
2016-03-30 13:34:14 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,10 +3,10 @@ $(document).ready(function()
     $(document).live("onchange",function()
     {
         if (screen.width<700) {//screen of mobile
-          document.getElementById('headerlogo').className = "headerlogom";
+          document.getElementById("headerlogo").className = "headerlogom";
         }
         else {//screen of desktop
-          document.getElementById('headerlogo').className = "headerlogod";
+          document.getElementById("headerlogo").className = "headerlogod";
         }
     });
 });
