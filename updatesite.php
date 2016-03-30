60bdc52b6a78349f2b7c681e5e6f47f68a890ae0
alexbob4444 <alexbob4444@gmail.com>
2016-03-29 22:33:17 -0500

    Update updatesite.php

--- updatesite.php
+++ updatesite.php
@@ -1,7 +1,7 @@
 <?php
 echo('0The code ran and maybe worked... ');
-echo shell_exec("/var/www/html/ git pull 2>&1");
-echo shell_exec('chmod +x shellupdate');
-echo shell_exec('./shellupdate');
+echo shell_exec("/var/www/html/dispersionfarms/ git pull 2>&1");
+echo shell_exec('/var/www/html/dispersionfarms/ chmod +x shellupdate');
+echo shell_exec('/var/www/html/dispersionfarms/ ./shellupdate');
 ?>
 
