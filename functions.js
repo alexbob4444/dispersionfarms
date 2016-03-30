1fd11005d5d56230b56bfa0470cb78db83995636
alexbob4444 <alexbob4444@gmail.com>
2016-03-30 13:57:32 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,12 +1,16 @@
 eventResize() {
     if (window.innerWidth<700) {//screen of mobile
       document.getElementById("headerlogo").className = "headerlogom";
+      
     }
     else {//screen of desktop
       document.getElementById("headerlogo").className = "headerlogod";
+      document.getElementById("mainbody").innerHTML = "These ran";
     }
+    document.getElementById("mainbody").innerHTML = "That ran";
 }
 
 window.onresize = eventResize();
 eventResize();
+document.getElementById("mainbody").innerHTML = "This ran";
 
