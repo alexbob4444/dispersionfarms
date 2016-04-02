72f46ba405cebc6059674b2976da7ca34dfab9cc
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 18:39:38 -0500

    Update createaccount.php

--- php/createaccount.php
+++ php/createaccount.php
@@ -3,6 +3,11 @@ if(isset($_POST['createsubmit'])) {
 echo $_POST['inputusername'];
 echo $_POST['inputpassword'];
 echo $_POST['retypepassword'];
+echo include('connect.php');
+if (!$con) {//bad connection
+	die("Cannot connect to Database: ". mysql_error());
+}
+mysql_select_db('dispersionfarms',$con);
 }
 //check createaccountform for inputusername, inputpassword, and retypepassword
 		//make sure inputusername doesn't already exist in database
