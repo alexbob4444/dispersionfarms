a7f82bd43996f0447fc4509910a1ae6a75e000db
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 17:17:07 -0500

    Update createaccount.php

--- php/createaccount.php
+++ php/createaccount.php
@@ -4,3 +4,9 @@ echo $_POST['inputpassword'];
 echo $_POST['retypepassword'];
 
 ?>
+//check createaccountform for inputusername, inputpassword, and retypepassword
+		//make sure inputusername doesn't already exist in database
+			//output that this account already exists to id=createaccountformmsg
+		//add to database
+		//create session
+		//display account info in id=dispaccount
