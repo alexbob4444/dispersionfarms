bf4c035853136e19d8381736b4e29d5fd8cd2a97
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 16:40:56 -0500

    Update myaccount.js

--- myaccount.js
+++ myaccount.js
@@ -11,14 +11,22 @@ function docready() {
 });
 $('#createaccountform').submit(function()
 {
-	//$.ajax({
+	$.ajax({
+		method:'POST',
+		url:'/php/createaccount.php',
+		data: {
+			
+		};
+		success: function(echosin) {
+			$('#dispaccount').innerHTML = echosin;
+		}
 		//check createaccountform for inputusername, inputpassword, and retypepassword
 		//make sure inputusername doesn't already exist in database
 			//output that this account already exists to id=createaccountformmsg
 		//add to database
 		//create session
 		//display account info in id=dispaccount
-	//});
+	});
 	return false;
 });
 }
