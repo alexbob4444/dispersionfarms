62a60308856674fe15fb26a2a60be7fd4667baa4
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 18:26:47 -0500

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <html>
 <?php
-echo "working"?
+echo "working?"
 //$con = mySQL_Connect("localhost","getdatauser","secret");
 //if (!$con) {//bad connection
 	//die("Cannot connect to Database: ", mysql_error());
