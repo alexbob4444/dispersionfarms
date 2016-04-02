10dd5aab86df34b77bbb6507042b546463fe391a
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 17:16:57 -0500

    Update myaccount.js

--- myaccount.js
+++ myaccount.js
@@ -1,38 +1,4 @@
-function docready() {
-  $('#loginform').submit(function()
-{
-	//$.ajax({
-		//check loginform for inputusername and inputpassword
-		//check against databased
-		//create session
-		//display account info in id=dispaccount
-	//});
-	return false;
-});
-$('#createaccountform').submit(function()
-{
-	$.ajax({
-		type:'POST',
-		url:'/php/createaccount.php',
-		data: {
-			
-		};
-		success: function(echoin) {
-			alert("data saved");
-			$('#dispaccount').innerHTML = echoin;
-		};
-		error: function() {
-			alert("failed");
-		}
-		//check createaccountform for inputusername, inputpassword, and retypepassword
-		//make sure inputusername doesn't already exist in database
-			//output that this account already exists to id=createaccountformmsg
-		//add to database
-		//create session
-		//display account info in id=dispaccount
-	});
-	return false;
-});
-}
+function sessionopencheck() {
 //check if session... if not openLogin()...
 //if so (document.getElementById("dispaccount").className = 'dispaccount';)
+}
