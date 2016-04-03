8b55aeaf883f8d66fc3ff617b98edebd2e810ef9
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 20:28:33 -0500

    Update createaccount.php

--- php/createaccount.php
+++ php/createaccount.php
@@ -21,7 +21,7 @@ if($record['username']==$usrn) {
 }
 
 if (!$userExistsAlready) {
-$sql = "INSERT INTO users (username,password) VALUES ('$usrn','$pass1')";
+$sql = "INSERT INTO users (username,password,datecreated) VALUES ('$usrn','$pass1','date()')";
 echo mysql_query($sql,$con);
 echo "Successful Creation";
 
