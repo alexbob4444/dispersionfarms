4d3fecc5750069d428f1b911f0376036d5eb1e01
alexbob4444 <alexbob4444@gmail.com>
2016-04-02 19:25:04 -0500

    Update createaccount.php

--- php/createaccount.php
+++ php/createaccount.php
@@ -3,7 +3,7 @@ if(isset($_POST['createsubmit'])) {//submit button pressed
 $usrn = $_POST['inputusername'];
 $pass1 = $_POST['inputpassword'];
 $pass2 = $_POST['retypepassword'];
-if(strcmp($pass1,$pass2)) {
+if(!strcmp($pass1,$pass2)) {
 include('connect.php');//connect to db
 if (!$con) {//bad connection
 	die("Cannot connect to Database: ". mysql_error());
@@ -12,18 +12,15 @@ else {//good connection
 $sql = "INSERT INTO users (username,password) VALUES ('$usrn','$pass1')";
 echo mysql_select_db('dispersionfarms',$con);
 echo mysql_query($sql,$con);
-echo "hi now";
+echo "Successful Creation";
 }
 include('closeconnect.php');
-echo "hi now1";
 }
 else {//passwords are not the same so reset page and note passwords need to be same
-	
-echo "hi now2";
+
 }
 }
 
-echo "hi now3";
 //check createaccountform for inputusername, inputpassword, and retypepassword
 		//make sure inputusername doesn't already exist in database
 			//output that this account already exists to id=createaccountformmsg
