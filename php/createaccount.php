ef8e21807f0d3d454d63d45f6c837a50a6faf5dc
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 15:00:03 -0500

    Update createaccount.php

--- php/createaccount.php
+++ php/createaccount.php
@@ -24,7 +24,6 @@ if (!$userExistsAlready) {
 $sql = "INSERT INTO users (username,password) VALUES ('$usrn','$pass1')";
 echo mysql_query($sql,$con);
 echo "Successful Creation";
-session_name('dispersionfarms');
 session_start();
 $_SESSION['currentuser']=$usrn;
 header('Location: http://www.dispersionfarms.com/myaccount');
