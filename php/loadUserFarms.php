236f88e6dcee808348829a254110b6068823092e
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 15:34:50 -0500

    Update loadUserFarms.php

--- php/loadUserFarms.php
+++ php/loadUserFarms.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
 $usrn = $_SESSION['currentuser'];
-
+include('connect.php');
+if (!$con) {
+  die("Cannot connect to Database: ". mysql_error());
+}
+else {
+  mysql_select_db('dispersionfarms',$con);
+  $sql = "";
+  mysql_query($sql,$con);
+}
 ?>
