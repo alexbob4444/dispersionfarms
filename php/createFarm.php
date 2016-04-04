4eb6878d80b70f66f93b47aa88cf823c748865a7
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 15:33:07 -0500

    Update createFarm.php

--- php/createFarm.php
+++ php/createFarm.php
@@ -1,4 +1,14 @@
 <?php
 session_start();
 $usrn = $_SESSION['currentuser'];
+include('connect.php');
+if (!$con) {
+  die("Cannot connect to Database: ". mysql_error());
+}
+else {
+  mysql_select_db('dispersionfarms',$con);
+  $sql = "INSERT INTO farm (owner,lat,lng,locationdescription,rainwater,plant1,plant2,plant3,plant4,plant5,dateplanted,public) VALUES ($usrn,$lat,$lng,$locdesc,$rainwater,$p1,$p2,$p3,$p4,$p5,$date,$public);
+  mysql_query($sql,$con);
+  header('Location: http://www.dispersionfarms.com/myaccount');
+}
 ?>
