3a770e9414b498975b125d659a0cb46100648c49
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 11:46:05 -0500

    Update login.php

--- php/login.php
+++ php/login.php
@@ -1,4 +1,35 @@
+<?php
+echo "compiled";
+if(isset($_POST['login']) {
+$usrn = $_POST['inputusername'];
+$pass = $_POST['inputpassword'];
+include('connect.php');//connect to db
+if (!$con) {//bad connection
+	die("Cannot connect to Database: ". mysql_error());
+}
+else {//good connection
+mysql_select_db('dispersionfarms',$con);//select the correct database
+$sql = "SELECT * FROM users where password='$pass' AND username='$usrn'";
+$rows = mysql_num_rows($sql);
+if(!($rows==1)) {
+	echo "password does not match username";
+}
+else {
+	//valid info was entered so login using session of $usrn
+	session_start();
+	$_SESSION['currentuser']=$usrn;
+	header('Location: http://www.dispersionfarms.com/myaccount');
+}
+
+include('closeconnect.php');
+}
+}
+else {
+	echo "Don't go to this page without using the interface buttons";
+}
 //check loginform for inputusername and inputpassword
 		//check against databased
 		//create session
 		//display account info in id=dispaccount
+		
+?>
