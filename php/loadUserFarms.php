e03d97bc98aba985f90ef9d1b87d287388a619ff
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 15:12:26 -0500

    Update loadUserFarms.php

--- php/loadUserFarms.php
+++ php/loadUserFarms.php
@@ -1,13 +1,5 @@
 <?php
 session_start();
 $usrn = $_SESSION['currentuser'];
-echo $usrn;
-//check if session...
-//if session has username matching a db username
-// then document.getElementById("dispaccount").className = 'dispaccount';
-//    and document.getElementById("createaccountform").className = 'createaccountform createaccountformhidden';
-//    and document.getElementById("loginform").className = 'loginform loginformhidden';
-
-//    format and echo all the data on that user, their sites, and their compost bins in a table with buttons to modify
 
 ?>
