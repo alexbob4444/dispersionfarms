9ccca0268a5d3b81d51fe688e80f93d056251100
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:29:30 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -150,7 +150,21 @@ function loadpubliccompostmap() {
 	//	draggable:false
 	//});
 	//marker1.setMap(map);
-	$.get('/dispersionfarms/php/mapPublicCompost.php');
+	"<?php\
+		include('connect.php');\
+		if (!$con) {//bad connection\
+			die("Cannot connect to Database: ". mysql_error());\
+		}else {\
+			mysql_select_db('dispersionfarms',$con);\
+			$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";\
+			$allbins = mysql_query($sql,$con);\
+			while($bin = mysql_fetch_array($allbins)) {\
+				echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng']),draggable:false});";\
+				echo "marker.setMap(map);";\
+			}\
+			include('closeconnect.php');\
+		}\
+	?>"
 	
 }
 function loadpublicfarmmap() {
