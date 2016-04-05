4c06d2f3350db1a67dbd88ab3b27824a24617fda
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:33:08 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -138,33 +138,12 @@ function closecreateobjectform() {
 	document.getElementById('requestdiv').className = 'requestdiv requestdivhidden';
 }
 function loadpubliccompostmap() {
-	document.getElementById('publiccompostdiv').style.width = '100%';
-	document.getElementById('publiccompostdiv').style.height = 400;
-	var map = new google.maps.Map(document.getElementById('publiccompostdiv'), {
-		zoom:13,
-		center: new google.maps.LatLng(43.070000,-89.411000),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	});
+	$.get('/dispersionfarms/php/mapPublicCompost.php');
 	//var marker1 = new google.maps.Marker({
 	//	position:new google.maps.LatLng(43.070000,-89.411000),
 	//	draggable:false
 	//});
 	//marker1.setMap(map);
-	"<?php\
-		include('connect.php');\
-		if (!$con) {//bad connection\
-			die("Cannot connect to Database: ". mysql_error());\
-		}else {\
-			mysql_select_db('dispersionfarms',$con);\
-			$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";\
-			$allbins = mysql_query($sql,$con);\
-			while($bin = mysql_fetch_array($allbins)) {\
-				echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng']),draggable:false});";\
-				echo "marker.setMap(map);";\
-			}\
-			include('closeconnect.php');\
-		}\
-	?>"
 	
 }
 function loadpublicfarmmap() {
