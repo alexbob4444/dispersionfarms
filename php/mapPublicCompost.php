251b09d06640559ddecfab0c569cd370b7411ba8
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:19:42 -0500

    Create mapPublicCompost.php

---
+++ php/mapPublicCompost.php
@@ -0,0 +1,15 @@
+<?php
+		include('connect.php');
+		if (!$con) {//bad connection
+			die("Cannot connect to Database: ". mysql_error());
+		}else {
+			mysql_select_db('dispersionfarms',$con);
+			$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";
+			$allbins = mysql_query($sql,$con);
+			while($bin = mysql_fetch_array($allbins)) {
+				echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng'),draggable:false});";
+				echo "marker.setMap(map);";
+			}
+			include('closeconnect.php');
+		}
+	?>
