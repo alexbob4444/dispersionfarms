b0c26e975d8b70e7c02f21de53776b7a9211da79
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 16:57:52 -0500

    Update numCompost.php

--- php/numCompost.php
+++ php/numCompost.php
@@ -1,7 +1,7 @@
 <?php  
 	include('connect.php'); 
 	mysql_select_db('dispersionfarms',$con); 
-	$sql = 'SELECT MAX(id) FROM compost';
+	$sql = "SELECT MAX(id) FROM compost WHERE public='1' AND requestfulfilled='1'";
 	$bin = mysql_fetch_row(mysql_query($sql,$con));
 	$val = $bin[0];
 	echo($val);
