dbe739d561fc019c2140030dd134485fb24f981c
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 21:03:04 -0500

    Update createFarm.php

--- php/createFarm.php
+++ php/createFarm.php
@@ -1,23 +1,23 @@
 <?php
 session_start();
 $usrn = $_SESSION['currentuser'];
-$lat = $_SESSION['currentuser'];
-$lng = $_SESSION['currentuser'];
-$locdesc = $_SESSION['currentuser'];
-$rainwater = $_SESSION['currentuser'];
+$lat = $_SESSION['lat'];
+$lng = $_SESSION['lng'];
+$locdesc = $_SESSION['locationdescriptiong'];
+$rainwater = $_SESSION['rain'];
 if ($rainwater=='yes') {
   $rainwater = true;
 }
 else {
   $rainwater =false;
 }
-$p1 = $_SESSION['currentuser'];
-$p2 = $_SESSION['currentuser'];
-$p3 = $_SESSION['currentuser'];
-$p4 = $_SESSION['currentuser'];
-$p5 = $_SESSION['currentuser'];
-$date = $_SESSION['currentuser'];
-$public = $_SESSION['currentuser'];
+$p1 = $_SESSION['p1'];
+$p2 = $_SESSION['p2'];
+$p3 = $_SESSION['p3'];
+$p4 = $_SESSION['p4'];
+$p5 = $_SESSION['p5'];
+$date = $_SESSION['date'];
+$public = $_SESSION['public'];
 if ($public=='yes') {
   $public = true;
 }
@@ -32,6 +32,7 @@ else {
   mysql_select_db('dispersionfarms',$con);
   $sql = "INSERT INTO farm (owner,lat,lng,locationdescription,rainwater,plant1,plant2,plant3,plant4,plant5,dateplanted,public) VALUES ($usrn,$lat,$lng,$locdesc,$rainwater,$p1,$p2,$p3,$p4,$p5,$date,$public)";
   mysql_query($sql,$con);
+  include('closeconnect.php');
   header('Location: http://www.dispersionfarms.com/myaccount');
 }
 ?>
