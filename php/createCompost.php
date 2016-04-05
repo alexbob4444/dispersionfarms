a5b02559f455d79c0386b2754cc82c52f4b5a4b8
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 21:42:58 -0500

    Update createCompost.php

--- php/createCompost.php
+++ php/createCompost.php
@@ -1,13 +1,28 @@
 <?php
 session_start();
 $usrn = $_SESSION['currentuser'];
+$lat = $_POST['lat'];
+$lng = $_POST['lng'];
+$locdesc = $_POST['locationdescription'];
+$public = $_POST['public'];
+if ($public=='yes') {
+  $public = true;
+}
+else {
+  $public =false;
+}
+echo $usrn;
+echo $lat;
+echo $lng;
+echo $locdesc;
+echo $public;
 include('connect.php');
 if (!$con) {
   die("Cannot connect to Database: ". mysql_error());
 }
 else {
   mysql_select_db('dispersionfarms',$con);
-  $sql = "INSERT INTO farm (owner,lat,lng,locationdescription,acceptsmeatoilfat,acceptsmanure,acceptsplantmatter,full,public,largedropoff) VALUES ($usrn,$lat,$lng,$locdesc,$mof,$m,$pm,$public,$largedropoff)";
+  $sql = "INSERT INTO farm (owner,lat,lng,locationdescription,public) VALUES ('$usrn','$lat','$lng','$locdesc','$public')";
   mysql_query($sql,$con);
   header('Location: http://www.dispersionfarms.com/myaccount');
 }
