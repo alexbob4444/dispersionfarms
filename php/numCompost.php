94acdd1df48dce64b691aa60332a6cd770132b89
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 11:53:13 -0500

    Update numCompost.php

--- php/numCompost.php
+++ php/numCompost.php
@@ -2,8 +2,8 @@
 	include('connect.php'); 
 	mysql_select_db('dispersionfarms',$con); 
 	$sql = 'SELECT MAX(id) FROM compost';
-	echo($bin = mysql_fetch_row(mysql_query($sql,$con)));
-	echo($val = $bin[0]);
+	$bin = mysql_fetch_row(mysql_query($sql,$con));
+	$val = $bin[0];
 	echo($val);
 	setcookie('numCompost',$val);
 ?>
