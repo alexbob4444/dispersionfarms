1c78731a0fdb686b21f48ef5a604d46a1ff9aa5d
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:22:42 -0500

    Update mapPublicCompost.php

--- php/mapPublicCompost.php
+++ php/mapPublicCompost.php
@@ -7,7 +7,7 @@
 			$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";
 			$allbins = mysql_query($sql,$con);
 			while($bin = mysql_fetch_array($allbins)) {
-				echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng'),draggable:false});";
+				echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng']),draggable:false});";
 				echo "marker.setMap(map);";
 			}
 			include('closeconnect.php');
