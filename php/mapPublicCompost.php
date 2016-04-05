fb11bacde79ed00c2cb9014dcee7f1a4663b0b5c
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:48:51 -0500

    Update mapPublicCompost.php

--- php/mapPublicCompost.php
+++ php/mapPublicCompost.php
@@ -6,11 +6,6 @@
 		mysql_select_db('dispersionfarms',$con);
 		$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";
 		$allbins = mysql_query($sql,$con);
-		
-		echo "<script type='text/javascript'>document.getElementById('publiccompostdiv').style.width = '100%';";
-		echo "document.getElementById('publiccompostdiv').style.height = 400;";
-		echo "var map = new google.maps.Map(document.getElementById('publiccompostdiv'), {zoom:13, center: new google.maps.LatLng(43.070000,-89.411000), mapTypeId: google.maps.MapTypeId.ROADMAP });";
-
 		while($bin = mysql_fetch_array($allbins)) {
 			echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng']),draggable:false});";
 			echo "marker.setMap(map);";
