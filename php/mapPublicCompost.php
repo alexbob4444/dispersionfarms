30eca2610eb8fdec95f52af95d5815b4768470ec
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 00:04:01 -0500

    Update mapPublicCompost.php

--- php/mapPublicCompost.php
+++ php/mapPublicCompost.php
@@ -4,7 +4,7 @@
 		die("Cannot connect to Database: ". mysql_error());
 	}else {
 		mysql_select_db('dispersionfarms',$con);
-		$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";
+		$sql = "SELECT * FROM compost WHERE public='1' AND requestfulfilled='1'";
 		$allbins = mysql_query($sql,$con);
 		while($bin = mysql_fetch_array($allbins)) {
 			echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng']),draggable:false});";
