99a1e1d82a673911c41338c6f7a491e46108a690
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:11:26 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -157,7 +157,7 @@ function loadpubliccompostmap() {
 		}else {
 			mysql_select_db('dispersionfarms',$con);
 			$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";
-			$bin = mysql_query($sql,$con);
+			$allbins = mysql_query($sql,$con);
 			while($bin = mysql_fetch_array($allbins)) {
 				echo "var marker = new google.maps.Marker({position:new google.maps.LatLng($bin['lat'],$bin['lng'),draggable:false});"
 				echo "marker.setMap(map);"
