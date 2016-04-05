5dc76541f186c1b31e9f742a054d2f8e39f4bfb8
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 11:43:27 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -149,9 +149,9 @@ function loadpubliccompostmap() {
 	include('connect.php'); \
 	mysql_select_db('dispersionfarms',$con); \
 	$sql = 'SELECT MAX(id) FROM compost';\
-	$bin = mysql_fetch_row(mysql_query($sql,$con)); \
-	$val = $bin[0];\
-	echo '$val'; \
+	echo($bin = mysql_fetch_row(mysql_query($sql,$con))); \
+	echo($val = $bin[0]);\
+	echo($val); \
 	?>";
 	document.getElementById('publicfarmdiv').innerHTML = numbins;
 	var currnum = 1;
