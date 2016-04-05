b7d72068e14234c45b11963705900ff1576cb53b
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 23:43:48 -0500

    Update mapPublicCompost.php

--- php/mapPublicCompost.php
+++ php/mapPublicCompost.php
@@ -7,7 +7,7 @@
 		$sql = "SELECT * FROM compost where public='1' AND requestfulfilled='1'";
 		$allbins = mysql_query($sql,$con);
 		
-		echo "<script>document.getElementById('publiccompostdiv').style.width = '100%';";
+		echo "<script type='text/javascript'>document.getElementById('publiccompostdiv').style.width = '100%';";
 		echo "document.getElementById('publiccompostdiv').style.height = 400;";
 		echo "var map = new google.maps.Map(document.getElementById('publiccompostdiv'), {zoom:13, center: new google.maps.LatLng(43.070000,-89.411000), mapTypeId: google.maps.MapTypeId.ROADMAP });";
 
