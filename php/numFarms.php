67d68836618e470dc1e4b6d5094ad66fd70b5c07
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 16:38:08 -0500

    Create numFarms.php

---
+++ php/numFarms.php
@@ -0,0 +1,9 @@
+<?php  
+	include('connect.php'); 
+	mysql_select_db('dispersionfarms',$con); 
+	$sql = 'SELECT MAX(id) FROM farm';
+	$bin = mysql_fetch_row(mysql_query($sql,$con));
+	$val = $bin[0];
+	echo($val);
+	include('closeconnect.php');
+?>
