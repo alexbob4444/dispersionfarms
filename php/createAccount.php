abe4bc30cc650deffb5201888bed5e0b50429796
alexbob4444 <alexbob4444@gmail.com>
2016-04-04 22:29:42 -0500

    Update createAccount.php

--- php/createAccount.php
+++ php/createAccount.php
@@ -4,6 +4,9 @@ if(isset($_POST['createsubmit'])) {//submit button pressed
 $usrn = $_POST['inputusername'];
 $pass1 = $_POST['inputpassword'];
 $pass2 = $_POST['retypepassword'];
+$name = $_POST['inputname'];
+$email = $_POST['inputemail'];
+$phone = $_POST['inputphone'];
 if($pass1==$pass2) {
 include('connect.php');//connect to db
 if (!$con) {//bad connection
@@ -15,7 +18,7 @@ $userExistsAlready = false;
 $sql = "SELECT * FROM users where username='$usrn'";
 $userData = mysql_query($sql,$con);
 if(!($rows==1)) {
-$sql = "INSERT INTO users (username,password) VALUES ('$usrn','$pass1')";
+$sql = "INSERT INTO users (username,password,name,email,phone) VALUES ('$usrn','$pass1','$name','$email','$phone')";
 echo mysql_query($sql,$con);
 echo "Successful Creation";
 session_start();
