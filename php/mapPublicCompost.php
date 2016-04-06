3dd0bbb3bf571fb511a0e5d9c4d16b63e1cbf705
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 19:35:31 -0500

    Update mapPublicCompost.php

--- php/mapPublicCompost.php
+++ php/mapPublicCompost.php
@@ -10,17 +10,18 @@
 		$sql = "SELECT * FROM compost WHERE public='1' AND requestfulfilled='1' AND id='$cid'";
 		$binquery = mysql_query($sql,$con);
 		$bin = mysql_fetch_array($binquery);
-		$sql = "SELECT * FROM users WHERE username='$bin['owner']'";
+		$owner=$bin['owner'];
+		$sql = "SELECT * FROM users WHERE username='$owner'";
 		$namequery = mysql_query($sql,$con);
 		$name = mysql_fetch_array($namequery);
-			echo(($bin['lat']));
-			echo(($bin['lng']));
-			if ($bin['lat']>10) {
+			echo($bin['lat']);
+			echo($bin['lng']);
+			if ($bin['lat'] > 10) {
 				echo "1";
 			}else {
 				echo "0";
 			}
-			echo("x7" . "Owner: " . $name['name'] . " Plants: " . $bin['plant1'] . $bin['plant2'] . $bin['plant3'] . $bin['plant4'] . $bin['plant5']. " Notes: " . $bin['locationdescription']); 
+			echo("x7" . "Owner: " . $name['name'] . " Plants: " . $bin['plant1'] . $bin['plant2'] . $bin['plant3'] . $bin['plant4'] . $bin['plant5'] . " Notes: " . $bin['locationdescription']); 
 	}
 	include('closeconnect.php');
 	}
