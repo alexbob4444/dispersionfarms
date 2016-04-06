91b6991e2c05adb2fb364f73fd23b4e0648b9b0b
alexbob4444 <alexbob4444@gmail.com>
2016-04-05 19:08:46 -0500

    Update mapPublicCompost.php

--- php/mapPublicCompost.php
+++ php/mapPublicCompost.php
@@ -9,7 +9,10 @@
 		mysql_select_db('dispersionfarms',$con);
 		$sql = "SELECT * FROM compost WHERE public='1' AND requestfulfilled='1' AND id='$cid'";
 		$binquery = mysql_query($sql,$con);
-		$bin = mysql_fetch_array($binquery);	
+		$bin = mysql_fetch_array($binquery);
+		$sql = "SELECT * FROM users WHERE username='$bin['owner']'"
+		$namequery = mysql_query($sql,$con);
+		$name = mysql_fetch_array($namequery);
 			echo(($bin['lat']));
 			echo(($bin['lng']));
 			if ($bin['lat']>10) {
@@ -17,6 +20,7 @@
 			}else {
 				echo "0";
 			}
+			echo("`*" ."Owner: " $name['name'] . " Plants: " . $bin['plant1'] . $bin['plant2'] . $bin['plant3'] . $bin['plant4'] . $bin['plant5']. " Notes: " . $bin['locationdescription']); 
 	}
 	include('closeconnect.php');
 	}
