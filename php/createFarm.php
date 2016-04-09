0115f63f5a8450c35517c40c6c7e352b44304a33
alexbob4444 <alexbob4444@gmail.com>
2016-04-09 01:22:42 -0500

    Update createFarm.php

--- php/createFarm.php
+++ php/createFarm.php
@@ -16,6 +16,8 @@ $p2 = $_POST['p2'];
 $p3 = $_POST['p3'];
 $p4 = $_POST['p4'];
 $p5 = $_POST['p5'];
+$p6 = $_POST['p6'];
+$p7 = $_POST['p7'];
 $date = $_POST['date'];
 $public = $_POST['public'];
 if ($public=='yes') {
@@ -31,7 +33,7 @@ if (!$con) {
 else {
   echo "Before Query";
   mysql_select_db('dispersionfarms',$con);
-  $sql = "INSERT INTO farm (owner,lat,lng,locationdescription,rainwater,plant1,plant2,plant3,plant4,plant5,dateplanted,public) VALUES ('$usrn','$lat','$lng','$locdesc','$rainwater','$p1','$p2','$p3','$p4','$p5','$date','$public')";
+  $sql = "INSERT INTO farm (owner,lat,lng,locationdescription,rainwater,plant1,plant2,plant3,plant4,plant5,plant6,plant7,dateplanted,public) VALUES ('$usrn','$lat','$lng','$locdesc','$rainwater','$p1','$p2','$p3','$p4','$p5','$p6','$p7','$date','$public')";
   echo mysql_query($sql,$con);
   include('closeconnect.php');
   header('Location: http://www.dispersionfarms.com/myaccount');
