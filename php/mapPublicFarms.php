eb292f6693cfa3760d4a5a9c9c1390ac22e0c344
alexbob4444 <alexbob4444@gmail.com>
2016-04-09 15:50:11 -0500

    Update mapPublicFarms.php

--- php/mapPublicFarms.php
+++ php/mapPublicFarms.php
@@ -9,6 +9,7 @@
 		mysql_select_db('dispersionfarms',$con);
 		$sql = "SELECT * FROM farm WHERE public='1' AND requestfulfilled='1' AND id='$cid'";
 		$binquery = mysql_query($sql,$con);
+		if (mysql_num_rows($query)>=1) {
 		$bin = mysql_fetch_array($binquery);	
 		$owner =$bin['owner'];
 		$sql = "SELECT * FROM users WHERE username='$owner'";
@@ -21,13 +22,15 @@
 			}else {
 				echo "0";
 			}
+			
 			echo("x7" . "<b>Owner:</b> " . $name['name'] . "<br>";
 			echo("<b>Plants:</b> " . $bin['plant1'])
 			for ($i=2; $i<8;$i++) {
 			  if ($bin["plant" . $i] != " ") 
-			  	echo(" & " . $bin['plant' . $i]);
+			  	echo(" & " . $bin[('plant' . $i)]);
 			}
-			echo(" Notes: " . $bin['locationdescription']); 
+			echo("<b>Notes:</b> " . $bin['locationdescription']); 
+		}
 	}
 	include('closeconnect.php');
 	}
