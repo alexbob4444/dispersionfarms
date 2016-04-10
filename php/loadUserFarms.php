b0349de3eb8550332c2c76efa707f8e2fdbe5c38
alexbob4444 <alexbob4444@gmail.com>
2016-04-10 15:42:22 -0500

    Update loadUserFarms.php

--- php/loadUserFarms.php
+++ php/loadUserFarms.php
@@ -11,7 +11,7 @@ else {
   $query = mysql_query($sql,$con);
   echo "<table style='width:100%'>";
   $rownum = 1;
-  echo "<tr style='height:30px;'><td>Descriptiong</td><td>Plant1</td><td>Plant2</td><td>Plant3</td><td>Plant4</td><td>Plant5</td><td>Plant6</td><td>Plant7</td><td>Public</td><td>DELETE</td></tr>";
+  echo "<tr style='height:30px;'><td>Description</td><td>Plant1</td><td>Plant2</td><td>Plant3</td><td>Plant4</td><td>Plant5</td><td>Plant6</td><td>Plant7</td><td>Public</td><td>DELETE</td></tr>";
   while ($row = mysql_fetch_assoc($result)) {
     $id=$row['id'];
     $ldesc=$row['locationdescription'];
@@ -26,6 +26,6 @@ else {
     echo "<tr style='height:30px;'><td>$ldesc</td><td>$p1</td><td>$p2</td><td>$p3</td><td>$p4</td><td>$p5</td><td>$p6</td><td>$p7</td><td>$pub</td><td><a href='#' onClick='deleteo('f',$id;'>DELETE</a></td></tr>";
     $rownum++;
   }
-  echo "</table>";
+  echo "</table><br>";
 }
 ?>
