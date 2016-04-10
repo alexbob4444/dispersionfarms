19607b27c888dad977660bd9ec7edb69cda50fff
alexbob4444 <alexbob4444@gmail.com>
2016-04-10 15:13:53 -0500

    Update loadUserFarms.php

--- php/loadUserFarms.php
+++ php/loadUserFarms.php
@@ -7,7 +7,14 @@ if (!$con) {
 }
 else {
   mysql_select_db('dispersionfarms',$con);
-  $sql = "";
-  mysql_query($sql,$con);
+  $sql = "SELECT * FROM compost WHERE owner='$usrn'";
+  $query = mysql_query($sql,$con);
+  echo "<table style='width:100%'>";
+  $rownum = 1;
+  for ($row = mysql_fetch_assoc($result)) {
+    echo "<tr style='height:30px;'><td id='fid'>$row['id']</td><td>$row['locationdescription']</td><td>$row['plant1']</td><td>$row['plant2']</td><td>$row['plant3']</td><td>$row['plant4']</td><td>$row['plant5']</td><td>$row['plant6']</td><td>$row['plant7']</td><td>$row['public']</td><td><a href='#' onClick='delete('f',$rownum)'>DELETE</a></td></tr>";
+    $rownum++;
+  }
+  echo "</table>"
 }
 ?>
