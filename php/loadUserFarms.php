2748df02e1d35bdde05d86753fbae9426fafc00a
alexbob4444 <alexbob4444@gmail.com>
2016-04-10 15:40:08 -0500

    Update loadUserFarms.php

--- php/loadUserFarms.php
+++ php/loadUserFarms.php
@@ -13,7 +13,17 @@ else {
   $rownum = 1;
   echo "<tr style='height:30px;'><td>Descriptiong</td><td>Plant1</td><td>Plant2</td><td>Plant3</td><td>Plant4</td><td>Plant5</td><td>Plant6</td><td>Plant7</td><td>Public</td><td>DELETE</td></tr>";
   while ($row = mysql_fetch_assoc($result)) {
-    echo "<tr style='height:30px;'><td>{$row['locationdescription']}</td><td>{$row['plant1']}</td><td>{$row['plant2']}</td><td>{$row['plant3']}</td><td>{$row['plant4']}</td><td>{$row['plant5']}</td><td>{$row['plant6']}</td><td>{$row['plant7']}</td><td>{$row['public']}</td><td><a href='#' onClick='deleteo('f',{$row['id']});'>DELETE</a></td></tr>";
+    $id=$row['id'];
+    $ldesc=$row['locationdescription'];
+    $p1=$row['plant1'];
+    $p2=$row['plant2'];
+    $p3=$row['plant3'];
+    $p4=$row['plant4'];
+    $p5=$row['plant5'];
+    $p6=$row['plant6'];
+    $p7=$row['plant7'];
+    $pub=$row['public'];
+    echo "<tr style='height:30px;'><td>$ldesc</td><td>$p1</td><td>$p2</td><td>$p3</td><td>$p4</td><td>$p5</td><td>$p6</td><td>$p7</td><td>$pub</td><td><a href='#' onClick='deleteo('f',$id;'>DELETE</a></td></tr>";
     $rownum++;
   }
   echo "</table>";
