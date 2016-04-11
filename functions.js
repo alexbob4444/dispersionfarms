6a99ccdf3b2096ffd2e92442de4f39ea6a68872c
alexbob4444 <alexbob4444@gmail.com>
2016-04-11 17:12:30 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -339,6 +339,7 @@ function loadPlant(plantname) {
 	//publicfarmdiv
 	var stringloc = "/dispersionfarms/textinfo/" + plantname + ".html";
 	document.getElementById('plantinfodiv').innerHTML=" ";
+	document.getElementById('plantinfodiv').style.display='block';
 	$.ajax({
 		url: stringloc,
 		dataType:"html",
@@ -353,7 +354,9 @@ function loadPlant(plantname) {
 		url: "/dispersionfarms/php/plantReady.php",
 		success: function (data) {
 			if (data == 1) {
-				document.getElementById('plantinfodiv').innerHTML="<h3>This Plant IS READY for harvest!</h3><br>"+document.getElementById('plantinfodiv').innerHTML;
+				document.getElementById('plantinfodiv').innerHTML="<div style='position:absolute;'>\
+				<h3>This Plant IS READY for harvest!</h3></div>\
+				<div><a href=\"javascript:document.getElementById('plantinfodiv').style.display='none';\">CLOSE</a></div>"+document.getElementById('plantinfodiv').innerHTML;
 			}
 			else {
 				document.getElementById('plantinfodiv').innerHTML="<h3>THIS PLANT IS NOT READY FOR HARVEST</h3><br>"+document.getElementById('plantinfodiv').innerHTML;
