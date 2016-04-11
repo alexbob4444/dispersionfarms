0109bf7dade49af1d1732d567a678e82178e25ad
alexbob4444 <alexbob4444@gmail.com>
2016-04-11 16:18:45 -0500

    Update plantReady.php

--- php/plantReady.php
+++ php/plantReady.php
@@ -2,7 +2,7 @@
 include('connect.php');
 $planttype=$_POST['planttype'];
 mysql_select_db('dispersionfarms',$con);
-$query = mysql_query("SELECT FROM plantready WHERE planttype='$planttype'",$con);
+$query = mysql_query("SELECT * FROM plantready WHERE planttype='$planttype'",$con);
 $row=mysql_fetch_array($query);
 echo $row['ready'];
 include('closeconnect.php');
