39cb525f982d2611928a35bbc596d10452b47405
alexbob4444 <alexbob4444@gmail.com>
2016-04-11 14:32:32 -0500

    Update loadUserCompost.php

--- php/loadUserCompost.php
+++ php/loadUserCompost.php
@@ -7,8 +7,30 @@ if (!$con) {
 }
 else {
   mysql_select_db('dispersionfarms',$con);
-  //$sql = "";
-  //mysql_query($sql,$con);
+  $sql = "SELECT * FROM compost WHERE owner='$usrn'";
+  $query = mysql_query($sql,$con);
+  if (mysql_num_rows($query) >=1) {
+  echo "<h2>Your Compost Bins</h2>";
+  echo "<table style='width:100%;opacity:0.75; border:1px solid grey;'>";
+  $rownum = 1;
+  echo "<tr><td><b>Description</b></td><td><b>LargeDropOff</b></td><td><b>MonthOpen</b></td><td><b>MonthClose</b></td><td><b>Public</b></td>c</b></td><td><b>Full</b></td><td><b>Recieved</b></td><td><b>DELETE</b></td></tr>";
+  while ($row = mysql_fetch_assoc($query)) {
+    $id=$row['id'];
+    $ldesc=$row['locationdescription'];
+    $ldropoff=$row['largedropoff'];
+    $start=$row['seasonstart'];
+    $end=$row['seasonend'];
+    $public=$row['public'];
+    $full=$row['full'];
+    $rf=$row['requestfulfilled'];
+    echo "<tr><td>$ldesc</td><td>$ldropoff</td><td>$start</td><td>$end</td><td>$public</td><td>$full</td><td>$rf</td><td><a href='#' onclick=\"deleteo('compost',$id)\">DELETE</a></td></tr>";
+    $rownum++;
+  }
+  echo "</table>";
+  }
+  else {
+    echo "You have no farms to display";
+  }
 }
 include('closeconnect.php');
 ?>
