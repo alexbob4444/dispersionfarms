6e95d64f5a83d9d84a6edaa3b29cf715ba6e6190
alexbob4444 <alexbob4444@gmail.com>
2016-04-13 16:01:47 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -359,12 +359,12 @@ function loadPlant(plantname) {
 		url: "/dispersionfarms/php/plantReady.php",
 		success: function (data) {
 			if (data == 1) {
-				document.getElementById('plantinfodiv').innerHTML="<a style='float:right;' href='#' onclick=\"javascript:document.getElementById('plantinfodiv').style.display='none';\" class='textlink'>CLOSE</a>\
+				document.getElementById('plantinfodiv').innerHTML="<a style='float:right top;' href='#' onclick=\"javascript:document.getElementById('plantinfodiv').style.display='none';\" class='textlink'>CLOSE</a>\
 				<h3>This Plant IS READY for harvest!</h3>"+document.getElementById('plantinfodiv').innerHTML;
 				window.scrollBy(0, 450);
 			}
 			else {
-				document.getElementById('plantinfodiv').innerHTML="<a style='float:right;' href='#' onclick=\"javascript:document.getElementById('plantinfodiv').style.display='none';\" class='textlink'>CLOSE</a>\
+				document.getElementById('plantinfodiv').innerHTML="<a style='float:right top;' href='#' onclick=\"javascript:document.getElementById('plantinfodiv').style.display='none';\" class='textlink'>CLOSE</a>\
 				<h3>This Plant IS NOT ready for harvest!</h3>"+document.getElementById('plantinfodiv').innerHTML;
 				window.scrollBy(0, 450);
 			}
