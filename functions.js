0f52afabaa40fe648d777822acd19e4bb15ed311
alexbob4444 <alexbob4444@gmail.com>
2016-04-18 09:20:10 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,8 +3,8 @@ function eventResize() {
 	document.getElementById('header').style.height = window.innerHeight*0.8;
 	document.getElementById('header').style.maxHeight = window.innerWidth*1.5;
 	var headerheight = parseInt(document.getElementById('header').style.height,10);
-	var textheight = parseInt(document.getElementById('pageheadtexts').style.height,10);
-	document.getElementById('pageheadtexts').style.top= (headerheight-130-textheight)/2 + "px";
+	//var textheight = parseInt(document.getElementById('pageheadtexts').style.height,10);
+	document.getElementById('pageheadtexts').style.top= (headerheight-130)/2 + "px";
     document.getElementById('pageheadtexts').innerHTML= (headerheight-130-textheight)/2 + "px";
     if (window.innerWidth<600) {//screen of mobile
       document.getElementById("headerlogoi").className = "headerlogom";
